432f54e7b2b9b4445725d85a5060c837adc98d24
Javier Leandro Arancibia <arancibiajav@gmail.com>
2017-11-13 19:11:16 +0100

    Initial

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,41 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
+import {
+	version
+} from '../../package.json';
+import {
+	Router
+} from 'express';
 import facets from './facets';
-
-export default ({ config, db }) => {
+import path from 'path';
+export default ({
+	config,
+	db
+}) => {
 	let api = Router();
 
 	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+	api.use('/facets', facets({
+		config,
+		db
+	}));
 
-	// perhaps expose some API metadata at the root
+	api.get('/download', (req, res) => {
+
+		var file = path.join(process.cwd(), "releases/slti-" + version+'-mac.zip');
+		res.download(file);
+	});
+
 	api.get('/', (req, res) => {
-		res.json({ version });
+
+		var IP = process.env.IP || "http://localhost:8081"
+
+		let rta = {
+			"url": IP + "/api/download",
+			"name": "1.0.5",
+			"notes": "",
+			"pub_date": "2017-11-13T16:13:58.000Z"
+		};
+
+		res.json(rta);
 	});
 
 	return api;
